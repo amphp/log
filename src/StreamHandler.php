abb86e45c903cb19e1d4cfc835b0ea18bdf3a525
Niklas Keller <me@kelunik.com>
2021-03-27 13:58:08 +0100

    Use revolt

--- src/StreamHandler.php
+++ src/StreamHandler.php
@@ -5,16 +5,10 @@ namespace Amp\Log;
 use Amp\ByteStream\OutputStream;
 use Monolog\Handler\AbstractProcessingHandler;
 use Psr\Log\LogLevel;
-use function Amp\async;
 
 final class StreamHandler extends AbstractProcessingHandler
 {
     private OutputStream $stream;
-
-    /** @var callable */
-    private $onResolve;
-
-    private ?\Throwable $exception = null;
 
     /**
      * @param OutputStream $outputStream
@@ -24,37 +18,19 @@ final class StreamHandler extends AbstractProcessingHandler
     public function __construct(OutputStream $outputStream, string $level = LogLevel::DEBUG, bool $bubble = true)
     {
         parent::__construct($level, $bubble);
+
         $this->stream = $outputStream;
-
-        $stream = &$this->stream;
-        $exception = &$this->exception;
-        $this->onResolve = static function (\Throwable $e = null) use (&$stream, &$exception) {
-            if (!$stream) {
-                return; // Prior write already failed, ignore this failure.
-            }
-
-            if ($e) {
-                $stream = null;
-                $exception = $e;
-
-                throw $e;
-            }
-        };
     }
 
     /**
      * Writes the record down to the log of the implementing handler.
      *
-     * @param  array $record
+     * @param array $record
      *
      * @return void
      */
     protected function write(array $record): void
     {
-        if ($this->exception) {
-            throw $this->exception;
-        }
-
-        async(fn() => $this->stream->write((string) $record['formatted']))->onResolve($this->onResolve);
+        $this->stream->write((string) $record['formatted']);
     }
 }
